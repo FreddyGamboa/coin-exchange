5e4bdc544701496446986804d1f8446da0619250
Freddy Alexandro Gamboa Velasco <freddy.g.velasco@gmail.com>
2020-02-06 14:16:04 -0600

    Primer verison

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+// eslint-disable-next-line
+import "@/assets/css/tailwind.css"
+import routes from '@/routes'
+import { dollarFilter, percentFilter } from '@/filters'
+import Chartick from 'vue-chartkick'
+import Chart from 'chart.js'
+import { VueSpinners } from '@saeris/vue-spinners'
 
-Vue.config.productionTip = false;
+//Filtros para ser utilizados en los componentes ver CoinDetail.vue
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+    router: routes,
+    render: h => h(App)
+}).$mount('#app')
